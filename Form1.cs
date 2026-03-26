78c0702371fcad8ea724bb1b6637c1468f3f2c44
지민 최 <jimin_0128@daum.net>
2026-03-26 10:54:50 +0900

    과제2 코딩미완성

--- Form1.cs
+++ Form1.cs
@@ -2,7 +2,7 @@ namespace SimpleCalculator
 {
     public partial class Form1 : Form
     {
-        // ===== 변수 =====
+        // ----- 변수 -----
         string currentInput = "";
         int firstNumber = 0;
         string op = "";
@@ -13,7 +13,7 @@ namespace SimpleCalculator
             InitializeComponent();
         }
 
-        // ===== 숫자 입력 공통 =====
+        // ----- 숫자 입력 공통 -----
         private void InputNumber(string num)
         {
             if (isNewInput)
@@ -28,7 +28,7 @@ namespace SimpleCalculator
             txtExpression.Text += num;
         }
 
-        // ===== 숫자 버튼 =====
+        // ----- 숫자 버튼 -----
         private void btn0_Click(object sender, EventArgs e) { InputNumber("0"); }
         private void btn1_Click(object sender, EventArgs e) { InputNumber("1"); }
         private void btn2_Click(object sender, EventArgs e) { InputNumber("2"); }
@@ -40,7 +40,7 @@ namespace SimpleCalculator
         private void btn8_Click(object sender, EventArgs e) { InputNumber("8"); }
         private void btn9_Click(object sender, EventArgs e) { InputNumber("9"); }
 
-        // ===== + 버튼 =====
+        // ----- + 버튼 -----
         private void btnpl_Click(object sender, EventArgs e)
         {
             if (currentInput == "") return;
@@ -52,13 +52,36 @@ namespace SimpleCalculator
             isNewInput = true;
         }
 
-        // ===== = 버튼 =====
+        // ----- = 버튼 -----
         private void btnEqual_Click(object sender, EventArgs e)
         {
             if (currentInput == "" || op == "") return;
 
             int secondNumber = int.Parse(currentInput);
-            int result = firstNumber + secondNumber;
+            int result = 0;
+
+            if (op == "+")
+            {
+                result = firstNumber + secondNumber;
+            }
+            else if (op == "-")
+            {
+                result = firstNumber - secondNumber;
+            }
+            else if (op == "*")
+            {
+                result = firstNumber * secondNumber;
+            }
+            else if (op == "/")
+            {
+                if (secondNumber == 0)
+                {
+                    MessageBox.Show("0으로 나눌 수 없습니다.");
+                    return;
+                }
+
+                result = firstNumber / secondNumber;
+            }
 
             // 아래: 결과만 표시
             txtResult.Text = result.ToString();
@@ -68,15 +91,50 @@ namespace SimpleCalculator
             isNewInput = true;
         }
 
-        // ===== 나머지 버튼 유지 =====
-        private void btnmin_Click(object sender, EventArgs e) { }
-        private void btnmul_Click(object sender, EventArgs e) { }
-        private void btndiv_Click(object sender, EventArgs e) { }
+        // ----- 나머지 버튼 유지 -----
+        private void btnmin_Click(object sender, EventArgs e)
+        {
+            if (currentInput == "") return;
+
+            firstNumber = int.Parse(currentInput);
+            op = "-";
+
+            txtExpression.Text += " - ";
+            isNewInput = true;
+        }
+
+        private void btnmul_Click(object sender, EventArgs e)
+        {
+            if (currentInput == "") return;
+
+            firstNumber = int.Parse(currentInput);
+            op = "*";
+
+            txtExpression.Text += " × ";
+            isNewInput = true;
+        }
+
+        private void btndiv_Click(object sender, EventArgs e)
+        {
+            if (currentInput == "") return;
+
+            firstNumber = int.Parse(currentInput);
+            op = "/";
+
+            txtExpression.Text += " ÷ ";
+            isNewInput = true;
+        }
+
         private void btndot_Click(object sender, EventArgs e) { }
+
         private void btnpm_Click(object sender, EventArgs e) { }
+
         private void C_Click(object sender, EventArgs e) { }
+
         private void CE_Click(object sender, EventArgs e) { }
+
         private void del_Click(object sender, EventArgs e) { }
+
         private void label1_Click(object sender, EventArgs e) { }
     }
 }
